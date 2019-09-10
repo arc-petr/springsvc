6805e77015f5845d8b8ce62ac30595cdf4ef7f72
Petr Igrevskiy <pigrevskiy@reputation.com>
2019-09-10 12:25:39 -0700

    Added AOP logging

---
+++ src/main/java/pvi/samplespring/log/LoggingManager.java
@@ -0,0 +1,33 @@
+package pvi.samplespring.log;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class LoggingManager {
+
+	private Logger libraryLogger = LogManager.getLogger(LoggingManager.class);
+
+	@Around("execution(* pvi.samplespring.*.*(..))")
+	public void coverMethod(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
+		String longName = proceedingJoinPoint.getSignature().toLongString();
+
+		libraryLogger.info(String.format("Method started: %s  ", longName));
+		try {
+
+			Object result = proceedingJoinPoint.proceed();
+			libraryLogger.info(String.format("Method %s returned %s  ", longName, result));
+		} catch (Throwable t) {
+			libraryLogger.info(String.format("Method %s throwed %s ", longName, t.getLocalizedMessage()));
+			throw t;
+		} finally {
+			libraryLogger.info(String.format("Method %s finished", longName));
+		}
+
+	}
+}
